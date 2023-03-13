2e260d7f5a1f20e12744e69abbc008d00d3bf5f8
vikneman <vika.florida@gmail.com>
2023-03-13 16:32:42 +0300

    Fix get_pet_labels function for systemic and hidden files

--- get_pet_labels.py
+++ get_pet_labels.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
-#
+#                                                                             
 # PROGRAMMER: Viktoriya P.
-# DATE CREATED: 03/10/2023
-# REVISED DATE:
-# PURPOSE: Create the function get_pet_labels that creates the pet labels from
-#          the image's filename. This function inputs:
-#           - The Image Folder as image_dir within get_pet_labels function and
-#             as in_arg.dir for the function call within the main function.
+# DATE CREATED: 03/10/2023                                  
+# REVISED DATE: 
+# PURPOSE: Create the function get_pet_labels that creates the pet labels from 
+#          the image's filename. This function inputs: 
+#           - The Image Folder as image_dir within get_pet_labels function and 
+#             as in_arg.dir for the function call within the main function. 
 #          This function creates and returns the results dictionary as results_dic
-#          within get_pet_labels function and as results within main.
+#          within get_pet_labels function and as results within main. 
 #          The results_dic dictionary has a 'key' that's the image filename and
 #          a 'value' that's a list. This list will contain the following item
 #          at index 0 : pet image label (string).
@@ -21,9 +21,9 @@ from os import listdir
 
 def get_pet_labels(image_dir):
     """
-    Creates a dictionary of pet labels (results_dic) based upon the filenames
-    of the image files. These pet image labels are used to check the accuracy
-    of the labels that are returned by the classifier function, since the
+    Creates a dictionary of pet labels (results_dic) based upon the filenames 
+    of the image files. These pet image labels are used to check the accuracy 
+    of the labels that are returned by the classifier function, since the 
     filenames of the images contain the true identity of the pet in the image.
     Be sure to format the pet labels so that they are in all lower case letters
     and with leading and trailing whitespace characters stripped from them.
@@ -32,29 +32,28 @@ def get_pet_labels(image_dir):
      image_dir - The (full) path to the folder of images that are to be
                  classified by the classifier function (string)
     Returns:
-      results_dic - Dictionary with 'key' as image filename and 'value' as a
+      results_dic - Dictionary with 'key' as image filename and 'value' as a 
       List. The list contains for following item:
          index 0 = pet image label (string)
     """
     filename_list = listdir(image_dir)
-
+    
     results_dic = dict()
-    items_in_dic = len(filename_list)
-    pet_labels = []
-
-
-    for name in filename_list:
-        dirty_label = name.lower().split("_")
-        label = ""
+    
+    
+    for filename in filename_list:
+        if filename[0] == ".":
+            continue
+            
+        dirty_label = filename.lower().split("_")
+        pet_label = ""
         for name_part in dirty_label:
             if name_part.isalpha():
-                label += name_part + " "
-        pet_labels.append(label.strip())
-
-    for idx in range(0, items_in_dic, 1):
-        if filename_list[idx] not in results_dic:
-             results_dic[filename_list[idx]] = [pet_labels[idx]]
+                pet_label += name_part + " "
+        
+        if filename not in results_dic:
+             results_dic[filename] = [pet_label.strip()]
         else:
-            print("** Warning: Key=", filename_list[idx],  "already exists in results_dic with value =",  results_dic[filename_list[idx]])
+            print("** Warning: Key=", filename,  "already exists in results_dic with value =",  results_dic[filename])
 
     return results_dic
